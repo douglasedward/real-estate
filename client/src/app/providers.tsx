c83351b5408bd825d008429988cc21303fe5b22a
douglasedward <douglaskyrius@gmail.com>
2025-03-13 23:06:55 -0400

    setting up cognito, amplify and auth provider

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { Authenticator } from "@aws-amplify/ui-react";
 import StoreProvider from "@/state/redux";
+import Auth from "./(auth)/authProvider";
 
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
 };
 
 export default Providers;
