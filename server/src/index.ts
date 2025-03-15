abb22b251268bf97fa95b7df5b97039e8dae3da7
douglasedward <douglaskyrius@gmail.com>
2025-03-14 22:23:57 -0400

    get and create endpoints for tenants and managers

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,17 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-// ROUTE IMPORT
+/* MIDDLEWARE */
+import { authMiddleware } from "./middleware/authMiddleware";
+/* ROUTE IMPORT */
+import tenantRoutes from "./routes/tenantRoutes";
+import managerRoutes from "./routes/managerRoutes";
 
-// CONFIGURATIONS
+/* CONSTS */
+const TENANT_ROLE = "tenant";
+const MANAGER_ROLE = "manager";
+
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -17,12 +25,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// ROUTES
+/* ROUTES */
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
-// SERVER
+app.use("/tenants", authMiddleware([TENANT_ROLE]), tenantRoutes);
+app.use("/managers", authMiddleware([MANAGER_ROLE]), managerRoutes);
+
+/* SERVER */
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
