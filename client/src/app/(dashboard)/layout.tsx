b3b60fa70701210aec6d238d770c93d35085b83c
douglasedward <douglaskyrius@gmail.com>
2025-03-15 22:07:23 -0400

    authentication-based redirects

---
+++ client/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Navbar from "@/components/Navbar";
+import AppSidebar from "@/components/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { useGetAuthUserQuery } from "@/state/api";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { usePathname, useRouter } from "next/navigation";
+
+const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { data: authUser, isLoading: isAuthLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/tenants")) ||
+        (userRole === "tenant" && pathname.startsWith("/managers"))
+      ) {
+        const redirectPath =
+          userRole === "manager"
+            ? "/managers/properties"
+            : "/tenants/favorites";
+        router.push(redirectPath, { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (isAuthLoading || isLoading) return <div>Loading...</div>;
+  if (!authUser?.userRole) return null;
+
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen w-full bg-primary-100">
+        <Navbar />
+        <div style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+          <main className="flex">
+            <AppSidebar userType={authUser.userRole?.toLowerCase()} />
+            <div className="flex-grow transition-all duration-300">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { data: authUser, isLoading: isAuthLoading } = useGetAuthUserQuery();
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (isAuthLoading || isLoading) return <div>Loading...</div>;
+
   return (
     <div className="h-full w-full">
       <Navbar />
