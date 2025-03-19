335cd7dbdeedc441fb50c3b20d512ceaa5e621c3
douglasedward <douglaskyrius@gmail.com>
2025-03-19 19:18:17 -0400

    add and remove favorite property, plus get all tenant residences

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -3,12 +3,18 @@ import {
   getTenant,
   createTenant,
   updateTenant,
+  getTenantProperties,
+  addFavoriteProperty,
+  removeFavoriteProperty,
 } from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
+router.get("/:cognitoId/properties", getTenantProperties);
 router.post("/", createTenant);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
 
 export default router;
