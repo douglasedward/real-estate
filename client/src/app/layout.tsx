321d30cdb7913f4aac5baced53533d188e220298
douglasedward <aiolosds@gmail.com>
2025-03-30 15:11:09 -0400

    showing toaster for notifications

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import Providers from "./providers";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <Toaster closeButton />
       </body>
     </html>
   );
