b25f61d346cac7774c48a2d70e85a76e27332bc1
douglasedward <aiolosds@gmail.com>
2025-03-31 18:14:21 -0400

    config files

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "real-estate",
+      scirpt: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
